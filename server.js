718b805177c0e6300767b8bb93a572b02c7aff0e
001.cconsule <cristiano.consule@rte.com.br>
2021-03-22 09:20:25 -0300

    Docker entre outros

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http")
-const server = http.createServer((req, res) => {
-    console.log("Pelo VSCode agora foi  com o yaml do google");
-    res.end("Pelo VSCode agora foi  com o yaml do google")
-})
-
-const PORT = process.env.PORT || 8080
-server.listen(PORT, () => console.log('Escutando 2'))
